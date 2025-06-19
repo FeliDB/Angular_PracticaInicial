1134581e9957ffef1d627f804a8e00c2702f655b
Felipe <felipediazb16@gmail.com>
2025-06-19 15:29:29 -0300

    Creacion pagina tabla delivery

---
+++ pedidos-ya/src/app/pages/table-delivery/table-delivery.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-delivery',
+  imports: [],
+  templateUrl: './table-delivery.html',
+  styleUrl: './table-delivery.css'
+})
+export class TableDelivery {
+  
+
+}
