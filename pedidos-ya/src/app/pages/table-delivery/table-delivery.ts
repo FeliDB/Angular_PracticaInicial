ccdfaf8411c57bed1da6ef36b2aead9bd4e30c63
Felipe <felipediazb16@gmail.com>
2025-06-23 14:42:20 -0300

    Comunicacion con el Back implementada

--- pedidos-ya/src/app/pages/table-delivery/table-delivery.ts
+++ pedidos-ya/src/app/pages/table-delivery/table-delivery.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-table-delivery',
-  imports: [],
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './table-delivery.html',
-  styleUrl: './table-delivery.css'
+  styleUrls: ['./table-delivery.css']
 })
 export class TableDelivery {
-  
+
+    deliveries: any[] = [];
+    estado: string = ""
+
+    constructor(private http: HttpClient) {
+        this.http.get<any[]>('http://localhost:3000/delivery').subscribe(data => {
+          this.deliveries = data;
+          console.log(this.deliveries);
+
+
+    });
+    }
+
+    getEstadoTraducido(status: string): string {
+    switch (status) {
+      case 'avaliable':
+        return 'Disponible';
+      case 'in_route':
+        return 'En Ruta';
+      default:
+        return status;
+    }
+  }
 
 }
